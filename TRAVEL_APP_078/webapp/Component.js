e11ecd205b1937d05d4a4bdc6826023d8669e714
Nero <ron.nero@sap.com>
2020-04-23 10:27:45 -0400

    initial commit

---
+++ TRAVEL_APP_078/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("namespace_078.travel_app_078.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
